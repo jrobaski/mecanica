5a81fb033687f0145ffa89fb14ca324fdca4893a
jrobaski <jrobaski@yahoo.com.br>
2017-05-19 22:58:28 -0300

    CRUD Estoque

--- src/main/resources/static/bootstrap/js/inter.js
+++ src/main/resources/static/bootstrap/js/inter.js
@@ -1,44 +1,44 @@
 $(document).ready(function(){
-        var braImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Brasilien.gif";
-    	var engImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Grossbritanien.gif";
-		
-		var imgBtnSel = $('#imgBtnSel');
-		var imgBtnBra = $('#imgBtnBra');
-		var imgBtnEng = $('#imgBtnEng');
-		
-		var imgNavSel = $('#imgNavSel');
-		var imgNavBra = $('#imgNavBra');
-		var imgNavEng = $('#imgNavEng');
-		
-		var spanNavSel = $('#lanNavSel');
-		var spanBtnSel = $('#lanBtnSel');
+	var braImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Brasilien.gif";
+	var engImgLink = "http://www.roemheld.de/IT/Data/Images/Address/Grossbritanien.gif";
 
-		imgBtnSel.attr("src",braImgLink);
-		imgBtnBra.attr("src",braImgLink);
-		imgBtnEng.attr("src",engImgLink);
-		
-		imgNavSel.attr("src",braImgLink);
-		imgNavBra.attr("src",braImgLink);
-		imgNavEng.attr("src",engImgLink);
-		
-		$( ".language" ).on( "click", function( event ) {
-			var currentId = $(this).attr('id');
+	var imgBtnSel = $('#imgBtnSel');
+	var imgBtnBra = $('#imgBtnBra');
+	var imgBtnEng = $('#imgBtnEng');
 
-			if(currentId == "navBra") {
-				imgNavSel.attr("src",braImgLink);
-				spanNavSel.text("BRA");
-			} else if (currentId == "navEng") {
-				imgNavSel.attr("src",engImgLink);
-				spanNavSel.text("ENG");
-			} 
+	var imgNavSel = $('#imgNavSel');
+	var imgNavBra = $('#imgNavBra');
+	var imgNavEng = $('#imgNavEng');
 
-			if(currentId == "btnBra") {
-				imgBtnSel.attr("src",braImgLink);
-				spanBtnSel.text("BRA");
-			} else if (currentId == "btnEng") {
-				imgBtnSel.attr("src",engImgLink);
-				spanBtnSel.text("ENG");
-			}
-			
-		});
+	var spanNavSel = $('#lanNavSel');
+	var spanBtnSel = $('#lanBtnSel');
+
+	imgBtnSel.attr("src",braImgLink);
+	imgBtnBra.attr("src",braImgLink);
+	imgBtnEng.attr("src",engImgLink);
+
+	imgNavSel.attr("src",braImgLink);
+	imgNavBra.attr("src",braImgLink);
+	imgNavEng.attr("src",engImgLink);
+
+	$( ".language" ).on( "click", function( event ) {
+		var currentId = $(this).attr('id');
+
+		if(currentId == "navBra") {
+			imgNavSel.attr("src",braImgLink);
+			spanNavSel.text("Bra");
+		} else if (currentId == "navEng") {
+			imgNavSel.attr("src",engImgLink);
+			spanNavSel.text("Eng");
+		} 
+
+		if(currentId == "btnBra") {
+			imgBtnSel.attr("src",braImgLink);
+			spanBtnSel.text("Bra");
+		} else if (currentId == "btnEng") {
+			imgBtnSel.attr("src",engImgLink);
+			spanBtnSel.text("Eng");
+		}
+
+	});
 });
